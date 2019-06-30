b776cc26120ce6bc252191fa66f19596e36537bc
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2019-06-30 20:39:06 +0545

    Added a module to reuse sockets for requests

---
+++ requestsSocketsReuse.py
@@ -0,0 +1,45 @@
+import requests
+
+class HTTPAdapterWithSocketOptions(requests.adapters.HTTPAdapter):
+    def __init__(self, *args, **kwargs):
+        self.socket_options = kwargs.pop("socket_options", None)
+        super(HTTPAdapterWithSocketOptions, self).__init__(*args, **kwargs)
+
+    def init_poolmanager(self, *args, **kwargs):
+        if self.socket_options is not None:
+            kwargs["socket_options"] = self.socket_options
+        super(HTTPAdapterWithSocketOptions, self).init_poolmanager(*args, **kwargs)
+
+def getSocket():
+    adapter = HTTPAdapterWithSocketOptions(socket_options=
+            [
+            (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            ])
+    s = requests.session()
+    s.mount("http://", adapter)
+    s.mount("https://", adapter)
+    return s
+
+def get(*args,**kwargs):
+    s = getSocket()
+    resp = s.get(*args,**kwargs)
+    s.close()
+    return resp
+
+def head(*args,**kwargs):
+    s = headSocket()
+    resp = s.head(*args,**kwargs)
+    s.close()
+    return resp
+
+def post(*args,**kwargs):
+    s = postSocket()
+    resp = s.post(*args,**kwargs)
+    s.close()
+    return resp
+
+def options(*args,**kwargs):
+    s = optionsSocket()
+    resp = s.options(*args,**kwargs)
+    s.close()
+    return resp
